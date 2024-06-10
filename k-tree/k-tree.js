e9f559373dd926dfab1ee7b4efe0ddb4318f1b60
Amanda <amandamarquez27@icloud.com>
2024-06-09 23:48:32 -0700

    Added test, not all are passing

--- k-tree/k-tree.js
+++ k-tree/k-tree.js
@@ -1,26 +1,55 @@
+const { KaryTree, Node } = require('../k-tree/kary-tree');
+
 function fizzBuzzTree(tree) {
-  if (!tree) return null;
-
-  const newTree = new KaryTree(tree.root.value.toString());
-
-  function traverse(node, newNode) {
-    for (let child of node.children) {
-      let newValue = '';
-      if (child.value % 3 === 0) {
-        newValue += 'Fizz';
-      }
-      if (child.value % 5 === 0) {
-        newValue += 'Buzz';
-      }
-      if (newValue === '') {
-        newValue = child.value.toString();
-      }
-      const newChild = newNode.addChild(newValue);
-      traverse(child, newChild);
-    }
+  if (!tree || !tree.root) {
+      return new KaryTree(null);
   }
 
-  traverse(tree.root, newTree.root);
+  const newRoot = new Node(fizzBuzzValue(tree.root.value));
+  const newTree = new KaryTree(newRoot);
+
+  function traverse(node, newNode) {
+      if (!node || !node.children) {
+          return;
+      }
+      for (let child of node.children) {
+          if (child && child.value !== null && child.value !== undefined) {
+              const newChild = new Node(fizzBuzzValue(child.value));
+              newNode.addChild(newChild);
+              traverse(child, newChild);
+          }
+      }
+  }
+
+  traverse(tree.root, newRoot);
 
   return newTree;
 }
+
+function fizzBuzzValue(value) {
+  if (value === null || value === undefined) {
+      return value;
+  }
+  let num = Math.abs(value);
+  let result;
+  if (num % 3 === 0 && num % 5 === 0) {
+      result = 'FizzBuzz';
+  } else if (num % 3 === 0) {
+      result = 'Fizz';
+  } else if (num % 5 === 0) {
+      result = 'Buzz';
+  } else {
+      result = num.toString();
+  }
+  // Handle negative numbers
+  if (value < 0) {
+      result = '-' + result;
+  }
+  return result;
+}
+
+
+
+
+module.exports = fizzBuzzTree;
+
